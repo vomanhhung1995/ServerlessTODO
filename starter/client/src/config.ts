0e94b33f9891313800539e81d1a03af503aafff2
vomanhhung1995 <vomanhhung1995@gmail.com>
2024-05-07 22:34:37 +0700

    "Update FE"

---
+++ starter/client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'fta6l96b1e'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-udacity-hungvm1.us.auth0.com', // Auth0 domain
+  clientId: 'XEHAyocAi41k3w8ZQAmUzwLUQfsCYeB7', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
